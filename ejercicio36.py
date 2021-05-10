22c91bff270d81b1701ad1cad74504a39693f1d8
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 09:58:01 -0500

    Add files via upload

---
+++ ejercicio36.py
@@ -0,0 +1,5 @@
+numero  =  int ( input ( "Ingrese un número menor a 100000:" ))
+si  numero  <  100000 :
+    print ( "El número" , "(" , numero , ")" , "tiene" , len ( str ( numero )), "dígitos" )
+otra cosa :
+    print ( "ERROR (el número es mayor a 100000)" )
